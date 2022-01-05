e72843e1d275aa7feedf34ba22e7bfce7e863585
Melissa Berumen <53724071+technic-angel@users.noreply.github.com>
2022-01-04 23:32:12 -0800

    3/4 done!

--- emilia.py
+++ emilia.py
@@ -12,11 +12,19 @@ Task 1 - Warmup
 
 
 @app.get("/task1/greet/{name}", tags=["Task 1"], summary="👋🇩🇪🇬🇧🇪🇸")
-async def task1_greet(name: str) -> str:
+async def task1_greet(name: str, language: str = 'de') -> str:
     """Greet somebody in German, English or Spanish!"""
     # Write your code below
     ...
-    return f"Hello {name}, I am Emilia."
+    print(name)
+    if language == 'en':
+        return f"Hello {name}, I am Emilia."
+    elif language == 'es':
+        return f'Hola {name}, soy Emilia.'
+    elif language == 'ita':
+        return f"Hallo {name}, leider spreche ich nicht 'ita'!"
+    else:
+        return f"Hallo {name}, ich bin Emilia."
 
 
 """
@@ -30,7 +38,21 @@ def camelize(key: str):
     """Takes string in snake_case format returns camelCase formatted version."""
     # Write your code below
     ...
-    return key
+    new_key = ""
+    cap = False
+    i = 0
+
+    while i < len(key):
+        if key[i] != '_':
+            if not cap:
+                new_key += key[i]
+            else:
+                new_key += key[i].upper()
+                cap = False
+        else:
+            cap = True
+        i += 1
+    return new_key
 
 
 @app.post("/task2/camelize", tags=["Task 2"], summary="🐍➡️🐪")
@@ -59,29 +81,63 @@ class ActionRequest(BaseModel):
 class ActionResponse(BaseModel):
     message: str
 
+#parser for caps and punctuation.
+def parser(message):
+    new_message = ""
+    punctuation = {'.', '?', '"', "'"}
+    i = 0
 
-def handle_call_action(action: str):
+    while i < len(message):
+        if message[i] not in punctuation:
+            new_message += message[i]
+
+        i += 1
+
+    return new_message.split()
+
+def path_selector(message):
+    for word in message:
+        if word.lower() == 'call':
+            return handle_call_action
+        elif word.lower() == 'remind':
+            return handle_reminder_action
+        elif word.lower() == 'timer':
+            return handle_timer_action
+    return handle_unknown_action
+
+
+
+
+def handle_call_action(action: list, username: str) -> ActionResponse:
     # Write your code below
     ...
-    return "🤙 Why don't you call them yourself!"
+    if username not in friends:
+        return {"message": f"Hi {username}, I don't know you yet. But I would love to meet you!"}
+    for word in action:
+        if word in friends[username]:
+            return {"message": f"🤙 Calling {word} ..."}
+
+    return {"message": f"{username}, I can't find this person in your contacts."}
 
 
-def handle_reminder_action(action: str):
+def handle_reminder_action(action: list, username: str) -> ActionResponse:
     # Write your code below
     ...
-    return "🔔 I can't even remember my own stuff!"
+    if username not in friends:
+        return {"message": f"Hi {username}, I don't know you yet. But I would love to meet you!"}
+    return {"message": f"🔔 Alright, I will remind you!"}
 
 
-def handle_timer_action(action: str):
+def handle_timer_action(action: list, username: str) -> ActionResponse:
     # Write your code below
     ...
-    return "⏰ I don't know how to read the clock!"
+    return {"message": f"⏰ Alright, the timer is set!"}
 
 
-def handle_unknown_action(action: str):
+def handle_unknown_action(action: list, username: str) -> ActionResponse:
     # Write your code below
     ...
-    return "🤬 #$!@"
+    return {"message": "👀 Sorry , but I can't help with that!"}
 
 
 @app.post("/task3/action", tags=["Task 3"], summary="🤌")
@@ -92,17 +148,18 @@ def task3_action(request: ActionRequest):
     # Write your code below
     ...
     from random import choice
-
     # There must be a better way!
-    handler = choice(
-        [
-            handle_call_action,
-            handle_reminder_action,
-            handle_timer_action,
-            handle_unknown_action,
-        ]
-    )
-    return handler(request.action)
+    # handler = choice(
+    #     [
+    #         handle_call_action,
+    #         handle_reminder_action,
+    #         handle_timer_action,
+    #         handle_unknown_action,
+    #     ]
+    #
+    action = parser(request.action)
+    handler = path_selector(action)
+    return handler(action, request.username)
 
 
 """
@@ -232,3 +289,5 @@ for i in 1, 2, 3, 4:
     tags = [f"Task {i}"]
     app.get(f"/task{i}", summary="📝", description=info(), tags=tags)(info)
     app.get(f"/task{i}/help", summary="🙋", description=help_(), tags=tags)(help_)
+
+
