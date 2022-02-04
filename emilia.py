49a2bf321dc14098d71cb55257b0a25d3b08d819
Max-McKone <mononoke@protonmail.ch>
2022-02-04 15:32:15 +0100

    task1 fix

--- emilia.py
+++ emilia.py
@@ -12,17 +12,17 @@ Task 1 - Warmup
 
 
 @app.get("/task1/greet/{name}", tags=["Task 1"], summary="👋🇩🇪🇬🇧🇪🇸")
-async def task1_greet(name: str) -> str:
+async def task1_greet(name: str, language : str ="de") -> str:
     """Greet somebody in German, English or Spanish!"""
     # Write your code below
-    if name=="Jasmin":
+    if language=="de":
         return f"Hallo {name}, ich bin Emilia."
-    elif name=="Stefan":
+    elif language=="en":
         return f"Hello {name}, I am Emilia."
-    elif name=="Hans":
+    elif language=="es":
         return f"Hola {name}, soy Emilia."
     else:
-        return f"Hallo Ben, leider spreche ich nicht 'ita'!"
+        return f"Hallo Ben, leider spreche ich nicht '{language}'!"
 
 
 
