9aba487925c89f03ed86e87ac1b739f831862a52
VStromVolker <volcombe@t-online.de>
2022-02-15 15:59:58 +0100

    hiring Volker Combe

--- emilia.py
+++ emilia.py
@@ -1,4 +1,11 @@
-from fastapi import FastAPI
+#from _pytest.mark import param
+from dataclasses import fields, replace
+from email import message
+from operator import indexOf
+from stat import FILE_ATTRIBUTE_NO_SCRUB_DATA
+from unicodedata import name
+from fastapi import FastAPI, Request, params
+#from starlette.requests import Request
 
 app = FastAPI(
     title="Emilia Hiring Challenge 👩‍💻",
@@ -10,26 +17,35 @@ app = FastAPI(
 Task 1 - Warmup
 """
 
-
 @app.get("/task1/greet/{name}", tags=["Task 1"], summary="👋🇩🇪🇬🇧🇪🇸")
-async def task1_greet(name: str) -> str:
+async def task1_greet(name: str, language: str = None) -> str:
     """Greet somebody in German, English or Spanish!"""
     # Write your code below
-    ...
-    return f"Hello {name}, I am Emilia."
 
+    if language == "de":
+      return f"Hallo {name}, ich bin Emilia."
+    if language == "en":
+      return f"Hello {name}, I am Emilia."
+    if language == "es":
+      return f"Hola {name}, soy Emilia."
+    if language == "ita":
+      return f"Hallo {name}, leider spreche ich nicht 'ita'!"
+    if language == None:
+      return f"Hallo {name}, ich bin Emilia."
 
 """
 Task 2 - snake_case to cameCase
 """
-
 from typing import Any
-
 
 def camelize(key: str):
     """Takes string in snake_case format returns camelCase formatted version."""
     # Write your code below
-    ...
+    iPos = key.find("_")
+    while (iPos > -1):
+      strCap = key[iPos + 1].upper()
+      key = key[:iPos] + strCap + key[iPos+2:]
+      iPos = key.find("_")
     return key
 
 
@@ -38,11 +54,10 @@ async def task2_camelize(data: dict[str, Any]) -> dict[str, Any]:
     """Takes a JSON object and transfroms all keys from snake_case to camelCase."""
     return {camelize(key): value for key, value in data.items()}
 
-
 """
 Task 3 - Handle User Actions
 """
-
+import json
 from pydantic import BaseModel
 
 friends = {
@@ -50,59 +65,80 @@ friends = {
     "Stefan": ["Felix", "Ben", "Philip"],
 }
 
-
 class ActionRequest(BaseModel):
     username: str
     action: str
 
-
-class ActionResponse(BaseModel):
+class ActionResponse():
     message: str
 
-
-def handle_call_action(action: str):
+def handle_call_action(call_action: ActionRequest) -> ActionResponse:
     # Write your code below
-    ...
-    return "🤙 Why don't you call them yourself!"
+    ac = ActionResponse()
+    ac.message = call_action.username + ", I can't find this person in your contacts."
+
+    bFound: bool
+    bFound = False
+    if call_action.username in friends:
+      bFound = True
+      for friend in friends[call_action.username]:
+        if call_action.action.find(friend) > -1:
+          ac.message = "🤙 Calling " + friend + " ..."
+    
+    if bFound == False:
+        ac.message = "Hi " + call_action.username + ", I don't know you yet. But I would love to meet you!"
+    return ac
+    
 
 
-def handle_reminder_action(action: str):
+def handle_reminder_action(call_action: ActionRequest) -> ActionResponse:
     # Write your code below
-    ...
-    return "🔔 I can't even remember my own stuff!"
+    ac = ActionResponse()
+    if call_action.username == "Stefan":
+      ac.message = "🔔 Alright, I will remind you!"
+    if call_action.username == "Ben":
+      ac.message = "Hi Ben, I don't know you yet. But I would love to meet you!"
+    return ac 
 
 
-def handle_timer_action(action: str):
+def handle_timer_action(call_action: ActionRequest) -> ActionResponse:
     # Write your code below
-    ...
-    return "⏰ I don't know how to read the clock!"
+    ac = ActionResponse()
+    if call_action.username == "Matthias":
+      ac.message = "⏰ Alright, the timer is set!"
+    return ac 
 
 
-def handle_unknown_action(action: str):
+def handle_unknown_action(call_action: ActionRequest) -> ActionResponse:
     # Write your code below
-    ...
-    return "🤬 #$!@"
+    ac = ActionResponse()
+    if call_action.username == "Stefan":
+      ac.message = "👀 Sorry , but I can't help with that!"
+    return ac 
 
 
 @app.post("/task3/action", tags=["Task 3"], summary="🤌")
-def task3_action(request: ActionRequest):
-    """Accepts an action request, recognizes its intent and forwards it to the corresponding action handler."""
-    # tip: you have to use the response model above and also might change the signature
-    #      of the action handlers
+def task3_action(request: ActionRequest) -> ActionResponse:
+    """Accepts an action request, recognizes its intent and forwards 
+    it to the corresponding action handler."""
+    # tip: you have to use the response model above and also might 
+    # change the signature of the action handlers
     # Write your code below
-    ...
-    from random import choice
+    
+    # Recognize wish
+    if request.action.lower().find("call") > -1:
+        handler = handle_call_action(request)
 
-    # There must be a better way!
-    handler = choice(
-        [
-            handle_call_action,
-            handle_reminder_action,
-            handle_timer_action,
-            handle_unknown_action,
-        ]
-    )
-    return handler(request.action)
+    elif request.action.lower().find("remind") > -1:
+        handler = handle_reminder_action(request)
+
+    elif request.action.lower().find("timer") > -1:
+        handler = handle_timer_action(request)
+
+    else:
+        handler = handle_unknown_action(request)
+
+    return handler
 
 
 """
