699d527a84518532dab9976a30f9de8c39099a5d
Umer Abbas <umerabbas@codexsquared.com>
2022-02-25 21:47:05 +0500

    task3 done

--- emilia.py
+++ emilia.py
@@ -85,49 +85,76 @@ class ActionResponse(BaseModel):
     message: str
 
 
-def handle_call_action(action: str):
+def handle_call_action(request: ActionRequest) -> ActionResponse:
     # Write your code below
-    ...
-    return "🤙 Why don't you call them yourself!"
+    friend = False
+    speech = request.action
+
+    for name in friends[request.username]:
+        if speech.lower().find(name.lower()) != -1:
+            friend = name
+            break
+
+    if friend in friends[request.username]:
+        return { 
+            'message': f"🤙 Calling {friend} ..." 
+        }
+
+    return { 
+        'message': f"{request.username}, I can't find this person in your contacts."
+    }
 
 
-def handle_reminder_action(action: str):
+def handle_reminder_action(request: ActionRequest) -> ActionResponse:
     # Write your code below
-    ...
-    return "🔔 I can't even remember my own stuff!"
+    return {
+        'message': "🔔 Alright, I will remind you!"
+    }
 
 
-def handle_timer_action(action: str):
+def handle_timer_action(request: ActionRequest) -> ActionResponse:
     # Write your code below
-    ...
-    return "⏰ I don't know how to read the clock!"
+    return {
+        'message': "⏰ Alright, the timer is set!"
+    }
 
 
-def handle_unknown_action(action: str):
+def handle_unknown_action(request: ActionRequest) -> ActionResponse:
     # Write your code below
-    ...
-    return "🤬 #$!@"
+    return {
+        'message': "👀 Sorry , but I can't help with that!"
+    }
 
 
 @app.post("/task3/action", tags=["Task 3"], summary="🤌")
-def task3_action(request: ActionRequest):
+def task3_action(request: ActionRequest) -> ActionResponse:
     """Accepts an action request, recognizes its intent and forwards it to the corresponding action handler."""
     # tip: you have to use the response model above and also might change the signature
     #      of the action handlers
     # Write your code below
-    ...
-    from random import choice
 
-    # There must be a better way!
-    handler = choice(
-        [
-            handle_call_action,
-            handle_reminder_action,
-            handle_timer_action,
-            handle_unknown_action,
-        ]
-    )
-    return handler(request.action)
+    if request.username not in friends:
+        return { 
+            'message': f"Hi {request.username}, I don't know you yet. But I would love to meet you!"
+        }
+
+    handler = handle_unknown_action
+    
+    availableActions = {
+        "call": handle_call_action, 
+        "remind": handle_reminder_action, 
+        "timer": handle_timer_action
+    }
+
+    speech = request.action.split(" ")
+
+    for word in speech:
+        for key, value in availableActions.items():
+            if word.lower().find(key) != -1:
+                handler = value
+                break
+    
+    return handler(request)
 
 
 """
