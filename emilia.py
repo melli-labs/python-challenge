d45c920295025e2b2b405e9767b644cbd9a8b4d8
Anand <anand@vindex.health>
2022-04-07 16:17:36 +0200

    Initial commit

--- emilia.py
+++ emilia.py
@@ -1,22 +1,32 @@
-from fastapi import FastAPI
+import logging
+
+from fastapi import FastAPI, Header
 
 app = FastAPI(
     title="Emilia Hiring Challenge 👩‍💻",
     description="Help Emilia 👩 to fix our tests and get a job interview 💼🎙️!",
 )
 
-
 """
 Task 1 - Warmup
 """
 
+from typing import Optional
+
 
 @app.get("/task1/greet/{name}", tags=["Task 1"], summary="👋🇩🇪🇬🇧🇪🇸")
-async def task1_greet(name: str) -> str:
+async def task1_greet(name: str, language: Optional[str] = None) -> str:
     """Greet somebody in German, English or Spanish!"""
     # Write your code below
-    ...
-    return f"Hello {name}, I am Emilia."
+
+    if language == "en":
+        return f"Hello {name}, I am Emilia."
+    elif language == "es":
+        return f"Hola {name}, soy Emilia."
+    elif language == "ita":
+        return f"Hallo {name}, leider spreche ich nicht 'ita'!"
+    else:
+        return f"Hallo {name}, ich bin Emilia."
 
 
 """
@@ -29,8 +39,11 @@ from typing import Any
 def camelize(key: str):
     """Takes string in snake_case format returns camelCase formatted version."""
     # Write your code below
-    ...
-    return key
+    if '_' in key:
+        key_lst = key.split('_')
+        for i in range(1, len(key_lst)):
+            key_lst[i] = key_lst[i].capitalize()
+    return ''.join(key_lst)
 
 
 @app.post("/task2/camelize", tags=["Task 2"], summary="🐍➡️🐪")
@@ -44,6 +57,8 @@ Task 3 - Handle User Actions
 """
 
 from pydantic import BaseModel
+from flair.data import Sentence
+from flair.models import SequenceTagger
 
 friends = {
     "Matthias": ["Sahar", "Franziska", "Hans"],
@@ -60,49 +75,58 @@ class ActionResponse(BaseModel):
     message: str
 
 
-def handle_call_action(action: str):
+def handle_call_action(name, nnp):
     # Write your code below
-    ...
-    return "🤙 Why don't you call them yourself!"
+    # check for the Proper noun listed in the values for the specific key.
+    if nnp in friends[name]:
+        return f"🤙 Calling {nnp} ..."
+    else:
+        return f"{name}, I can't find this person in your contacts."
 
 
-def handle_reminder_action(action: str):
-    # Write your code below
-    ...
-    return "🔔 I can't even remember my own stuff!"
+def get_nnp_vb(text: str):
+    # load tagger
+    tagger = SequenceTagger.load("flair/pos-english")
+    # make sentence
+    sentence = Sentence(text)
+    # predict NER tags
+    tagger.predict(sentence)
+    nnp = None
+    verb = []
+    for i in sentence.tokens:
+        if i.tag == 'NNP':
+            nnp = i.form
+        elif i.tag == 'VB':
+            vb = i.form
+            verb.append(vb.lower())
+    return nnp, verb
 
 
-def handle_timer_action(action: str):
-    # Write your code below
-    ...
-    return "⏰ I don't know how to read the clock!"
-
-
-def handle_unknown_action(action: str):
-    # Write your code below
-    ...
-    return "🤬 #$!@"
-
-
-@app.post("/task3/action", tags=["Task 3"], summary="🤌")
+@app.post("/task3/action", tags=["Task 3"], summary="🤌", response_model=ActionResponse)
 def task3_action(request: ActionRequest):
     """Accepts an action request, recognizes its intent and forwards it to the corresponding action handler."""
     # tip: you have to use the response model above and also might change the signature
     #      of the action handlers
-    # Write your code below
-    ...
-    from random import choice
+    user_name = request.username
+    action = request.action
 
-    # There must be a better way!
-    handler = choice(
-        [
-            handle_call_action,
-            handle_reminder_action,
-            handle_timer_action,
-            handle_unknown_action,
-        ]
-    )
-    return handler(request.action)
+    # check for the user_name key in the friends dict
+    if user_name in friends.keys():
+        # returns the Proper noun and verb using flair from huggingface
+        proper_noun, verb = get_nnp_vb(action)
+
+        if verb:
+            if 'call' in verb:
+                message = handle_call_action(user_name, proper_noun)
+                return ActionResponse(message=message)
+            elif 'remind' in verb:
+                return ActionResponse(message="🔔 Alright, I will remind you!")
+            elif 'set' in verb:
+                return ActionResponse(message="⏰ Alright, the timer is set!")
+        else:
+            return ActionResponse(message="👀 Sorry , but I can't help with that!")
+    else:
+        return ActionResponse(message=f"Hi {user_name}, I don't know you yet. But I would love to meet you!")
 
 
 """
@@ -154,6 +178,10 @@ class User(BaseModel):
     secret: str
 
 
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+
 class Token(BaseModel):
     access_token: str
     token_type: str
@@ -165,16 +193,31 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     # fixme 🔨, at the moment we allow everybody to obtain a token
     # this is probably not very secure 🛡️ ...
     # tip: check the verify_password above
-    # Write your code below
-    ...
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
     payload = {
         "sub": form_data.username,
         "exp": datetime.utcnow() + timedelta(minutes=30),
     }
+
     return {
         "access_token": encode_jwt(payload),
         "token_type": "bearer",
     }
+
+
+def authenticate_user(username: str, password: str):
+    user = get_user(username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
 
 
 def get_user(username: str) -> Optional[User]:
@@ -191,20 +234,30 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
     )
     # check if the token 🪙 is valid and return a user as specified by the tokens payload
     # otherwise raise the credentials_exception above
-    # Write your code below
-    ...
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALOGRITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
 
 
 @app.get("/task4/users/{username}/secret", summary="🤫", tags=["Task 4"])
-async def read_user_secret(
-    username: str, current_user: User = Depends(get_current_user)
-):
+async def read_user_secret(username: str, current_user: User = Depends(get_current_user)):
     """Read a user's secret."""
     # uppps 🤭 maybe we should check if the requested secret actually belongs to the user
-    # Write your code below
-    ...
-    if user := get_user(username):
+    user = get_user(username)
+    # check the current user is the actual user by checking username and password
+    if current_user.username is user.username and current_user.hashed_password is user.hashed_password:
         return user.secret
+    else:
+        raise HTTPException(status_code=403, detail="Don't spy on other user!")
 
 
 """
