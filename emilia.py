c497f408a95f2323a3862a22107ab42fcb42ca96
vadim <vadim17@hotmail.de>
2022-06-09 13:46:41 +0200

    solve task 1

--- emilia.py
+++ emilia.py
@@ -12,11 +12,16 @@ Task 1 - Warmup
 
 
 @app.get("/task1/greet/{name}", tags=["Task 1"], summary="👋🇩🇪🇬🇧🇪🇸")
-async def task1_greet(name: str) -> str:
+async def task1_greet(name: str, language: str = "de") -> str:
     """Greet somebody in German, English or Spanish!"""
     # Write your code below
     ...
-    return f"Hello {name}, I am Emilia."
+    greetings = {
+        "de": f"Hallo {name}, ich bin Emilia.",
+        "en": f"Hello {name}, I am Emilia.",
+        "es": f"Hola {name}, soy Emilia.",
+    }
+    return greetings.get(language, f"Hallo {name}, leider spreche ich nicht '{language}'!")
 
 
 """
