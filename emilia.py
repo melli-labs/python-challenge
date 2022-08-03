aa800e594ee49b2184f266fdcd934977196d77d5
Can <>
2022-08-03 13:59:35 +0200

    task1

--- emilia.py
+++ emilia.py
@@ -12,11 +12,18 @@ Task 1 - Warmup
 
 
 @app.get("/task1/greet/{name}", tags=["Task 1"], summary="👋🇩🇪🇬🇧🇪🇸")
-async def task1_greet(name: str) -> str:
+async def task1_greet(name: str, language: str = "de") -> str:
     """Greet somebody in German, English or Spanish!"""
     # Write your code below
-    ...
-    return f"Hello {name}, I am Emilia."
+    if language in ["en", "es", "de"]:
+        if language == "en":
+            return f"Hello {name}, I am Emilia."
+        elif language == "es":
+            return f"Hola {name}, soy Emilia."
+        elif language == "de":
+            return f"Hallo {name}, ich bin Emilia."
+    else:
+        return f"Hallo Ben, leider spreche ich nicht '{language}'!"
 
 
 """
